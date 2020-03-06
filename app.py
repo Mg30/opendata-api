ea5cfa42de440f614404b14a459563454440cc92
matt <matgonzalez736@gmail.com>
2020-03-06 16:30:49 +0100

    update dependancies
    update dependancies

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 from services.scrapping import Marmiton, InfoClimat
-from services.classifiers import ChurnClassifier
 from flask_restful_swagger import swagger
 
 app = Flask(__name__)
@@ -10,9 +9,8 @@ CORS(app)
 api =  swagger.docs(Api(app))
 
 
-api.add_resource(Marmiton, "/api/scrapping/marmiton/fetch/ingredients")
-api.add_resource(ChurnClassifier, "/api/ml/predict/churn")
+api.add_resource(Marmiton, "/api/scrapping/marmiton/fetch/ingredients"))
 api.add_resource(InfoClimat, "/api/info-climat/auth")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
